2d4356b9d21dfadcd7bc1aefaa62acb0edcfea0e
MT Scripts <89866234+Marttins011@users.noreply.github.com>
2022-03-19 03:28:58 +0000

    Some tests

--- mt-coke/server.lua
+++ mt-coke/server.lua
@@ -18,12 +18,12 @@ RegisterNetEvent('mt-coke:server:ProcessarCoke', function()
             Player.Functions.RemoveItem('coke_empty_bags', 30)
             Player.Functions.AddItem('coke_bags', 30)
             TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['coke_bags'], "add")
-            QBCore.Functions.Notify('Cocaine processed!', 'success')
+           -- QBCore.Functions.Notify('Cocaine processed!', 'success')
         else
-            QBCore.Functions.Notify('You do not have the rigth items!', 'error')
+            --QBCore.Functions.Notify('You do not have the rigth items!', 'error')
         end
     else
-        QBCore.Functions.Notify('you need atleast 30 Coke Empty Bags and 20 Coke Leafs!', 'error')
+       -- QBCore.Functions.Notify('you need atleast 30 Coke Empty Bags and 20 Coke Leafs!', 'error')
     end
 end)
 
